74b621c48dde661aa1167419f096a4229359df67
vdm-dev <vdm-dev@users.noreply.github.com>
2017-02-23 21:06:07 +0400

    Added license information

--- src/Url.h
+++ src/Url.h
@@ -1,5 +1,6 @@
 //
 //  Copyright (c) 2017 Dmitry Lavygin (vdm.inbox@gmail.com)
+//  Copyright (c) 2015 Oleg Morozenkov
 // 
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -21,48 +22,43 @@
 //
 
 
-#ifndef StdAfx_INCLUDED
-#define StdAfx_INCLUDED
-
-#ifdef _WIN32
-#include <sdkddkver.h>
-#endif
+#ifndef Url_INCLUDED
+#define Url_INCLUDED
 
 
-#include <cstdio>
-#include <cstdint>
-#include <cwchar>
-
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <list>
-#include <vector>
-#include <map>
-
-#include <boost/algorithm/string.hpp>
-#include <boost/asio.hpp>
-#include <boost/asio/ssl.hpp>
-#include <boost/enable_shared_from_this.hpp>
-#include <boost/filesystem.hpp>
-#include <boost/foreach.hpp>
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/console.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/support/date_time.hpp>
-#include <boost/program_options.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
-#include <boost/range.hpp>
 
 
-using namespace boost;
+class Url
+{
+public:
+	Url(const std::string& url);
+
+	/**
+	 * Protocol part of an url. Example: https://
+	 */
+	std::string protocol;
+
+	/**
+	 * Host part of an url. Example: www.example.com
+	 */
+	std::string host;
+
+	/**
+	 * Path part of an url including preceding '/' char. Example: /index.html
+	 */
+	std::string path;
+
+	/**
+	 * Query part of an url without '?' char. Example: a=1&b=2&c=3
+	 */
+	std::string query;
+
+	/**
+	 * Fragment part of an url without '#' char. Example: section1
+	 */
+	std::string fragment;
+};
 
 
-#endif // StdAfx_INCLUDED
+#endif // Url_INCLUDED
